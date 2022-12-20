f6a54a353a86fecf6ed2e3853198eed19a3de7da
git-ainya <miko3515@gmail.com>
2022-12-20 07:20:01 +0000

    Update src.a2b27638.js

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -203,7 +203,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36551" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46357" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
